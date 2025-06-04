7b1e981be9c8f8323b98c805eed3f20c22ebfde3
MSI\mati <matimichalski01@gmail.com>
2025-06-04 23:55:08 +0200

    Dodana klasa Gracz z metodami i statystykami

--- text-rpg/text-rpg.cpp
+++ text-rpg/text-rpg.cpp
@@ -1,4 +1,43 @@
-﻿//coming soon
+﻿#include <iostream>
+
+using namespace std;
+
+class Gracz {
+private:
+   string name;
+   int hp;
+   int stamina;
+   int dmg;
+public:
+   Gracz(int h = 100, int s = 50, int d = 10) {
+       hp = h;
+       stamina = s;
+       dmg = d;
+   }
 
 
+   string GetName() {
+       return name;
+   }
 
+   void SetName(string s) {
+       name = s;
+   }
+
+   void Statystyki() {
+       cout << "imie:" << name << endl << "zdrowie" << hp << endl << "stamina:" << stamina << endl << "obrazenia:" << dmg << endl;
+   }
+  
+};
+
+int main() {
+   Gracz gracz;
+
+   cout << "Podaj imie" << endl;
+   string s_name;
+   cin >> s_name;
+   gracz.SetName(s_name);
+   gracz.Statystyki();
+
+   return 0;
+}
